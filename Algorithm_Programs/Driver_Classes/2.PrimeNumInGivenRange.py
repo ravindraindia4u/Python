a41902add6484486fe6d6f02efa1081378c4cf3f
ravindraindia4u <ravindraindia4u@gmail.com>
2020-04-27 12:59:43 +0530

    prime number in given range

---
+++ Algorithm_Programs/Driver_Classes/2.PrimeNumInGivenRange.py
@@ -0,0 +1,6 @@
+from Algorithm_Programs.Util import Util
+
+start = int(input("Enter starting Range: "))
+end = int(input("Enter ending Range: "))
+print("prime number between", start, "to", end, "are:", end=" ")
+Util.prime(start, end)
--- Algorithm_Programs/Util.py
+++ Algorithm_Programs/Util.py
@@ -6,3 +6,16 @@ class Util:
             return True
         else:
             return False
+    # Function to find prime number in given range
+    @staticmethod
+    def prime(start, end):
+        list = []
+        for i in range(start, end):
+            if i == 1 or i == 0:
+                continue
+            count = 0
+            for j in range(2, i):
+                if i % j == 0:
+                    count += 1
+            if count == 0 and i != 1:
+                print(i, end=" ")
