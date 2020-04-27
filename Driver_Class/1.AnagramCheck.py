79c7d8d54ac45056275194ea7e2ccea7968d2616
Ravindra Kumar Sah <ravindraindia4u@gmail.com>
2020-04-27 12:14:03 +0530

    Delete 1.AnagramCheck.py

--- Driver_Class/1.AnagramCheck.py
+++
@@ -1,8 +0,0 @@
-from Algorithm_Programs.Util import Util
-
-string1 = input("Enter first string: ")
-string2 = input("Enter second string: ")
-if Util.anagram(string1, string2):
-    print(string1, "and", string2, "are anagram.")
-else:
-    print(string1, "and", string2, "are not anagram.")
