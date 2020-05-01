4c556f11b0de754f03efe6262f6e0b7c66ef2446
ravindraindia4u <ravindraindia4u@gmail.com>
2020-05-01 18:47:04 +0530

    Regular_Expression_Demonstration desc update

--- Object_Oriented_Programs/Regular_Expression_Demonstration.py
+++ Object_Oriented_Programs/Regular_Expression_Demonstration.py
@@ -1,4 +1,9 @@
-
+"""
+Read in the following message: Hello <<name>>, We have your full
+name as <<full name>> in our system. your contact number is 91­xxxxxxxxxx.
+Please,let us know in case of any clarification Thank you BridgeLabz 01/01/2016.
+Use Regex to replace name, full name, Mobile#, and Date with proper value.
+"""
 import re
 from datetime import datetime
 
